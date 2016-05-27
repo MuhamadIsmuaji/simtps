8d452b040e8b916de213732a3ad3d7cbd3690ce2
MuhamadIsmuaji <muhamadismuaji@gmail.com>
2016-05-27 10:23:53 +0700

    Perbaikan jadwal sidang dihalaman peserta

--- application/controllers/lecturer/data/Schedule.php
+++ application/controllers/lecturer/data/Schedule.php
@@ -78,46 +78,6 @@ class Schedule extends CI_Controller {
 		$this->load->view('template',$data);
 	}
 
-	public function hearingScheduleDetail($thn_ajaran = NULL, $smt = NULL, $ruang = NULL, $tgl = NULL, $mulai = NULL, $akhir = NULL) {
-		if ( !$this->session->userdata('isLecturerTps') && !$this->session->userdata('isAdminTps') ) {
-			redirect('public/home','refresh');
-		}
-
-		if ( $thn_ajaran == NULL || $smt == NULL || $ruang == NULL || $tgl == NULL || $mulai == NULL || $akhir == NULL  ) {
-			redirect('lecturer/data/schedule/hearingSchedule','refresh');
-		}
-
-		$setting = $this->M_setting->getSetting()->row();
-
-		if ( $thn_ajaran != $setting->thn_ajaran && $thn_ajaran != $setting->smt ) {
-			redirect('lecturer/data/schedule/hearingSchedule','refresh');
-		}
-
-		$identitasJadwal = $this->M_jadwal->getJadwalByIdentitas($thn_ajaran,$smt,$ruang,$tgl,$mulai,$akhir)->row();
-		$detailJadwal = $this->M_jadwal->getJadwalByIdentitas($thn_ajaran,$smt,$ruang,$tgl,$mulai,$akhir)->result();
-		if ( !$identitasJadwal || !$detailJadwal ) {
-			redirect('lecturer/data/schedule/hearingSchedule','refresh');
-		}
-
-		$moderator = $this->M_dosen->getDosenByNpp($identitasJadwal->moderator)->row()->nama;
-		$penguji1 = $this->M_dosen->getDosenByNpp($identitasJadwal->penguji1)->row()->nama;
-		$penguji2 = $this->M_dosen->getDosenByNpp($identitasJadwal->penguji2)->row()->nama;
-
-		$data = [
-			'settingData'		=> $setting,
-			'moderator'			=> $moderator,
-			'penguji1'			=> $penguji1,
-			'penguji2'			=> $penguji2,
-			'identitasJadwal'	=> $identitasJadwal,
-			'detailJadwal'		=> $detailJadwal,
-			'content' 			=> 'lecturer/data/schedule/hearing_schedule_detail',
-			'pagetitle' 		=> 'Detail Jadwal Sidang',
-			'navbartitle' 		=> 'Detail Jadwal Sidang'
-		];
-
-		$this->load->view('template',$data);
-	}
-
 }
 
 /* End of file Schedule.php */
--- application/controllers/participant/Schedule.php
+++ application/controllers/participant/Schedule.php
@@ -21,59 +21,66 @@ class Schedule extends CI_Controller {
 		}
 
 		$setting = $this->M_setting->getSetting()->row();
-		$jadwal = $this->M_jadwal->getScheduleList($setting->thn_ajaran,$setting->smt)->result();
+		$thn_ajaran = $setting->thn_ajaran;
+		$smt = $setting->smt;
+		$kode_kel = $this->M_anggota->getAnggotaByPeriode($thn_ajaran,$smt,$this->session->userdata('nbi'))->row()->kode_kel;
 
-		$data = [
-			'settingData'	=> $setting,
-			'jadwal'		=> $jadwal,
-			'content' 		=> 'participant/schedule/hearing_schedule_lists',
-			'pagetitle' 	=> 'Jadwal Sidang',
-			'navbartitle' 	=> 'Jadwal Sidang'
-		];
+		$jad_kelompok = $this->M_jadwal->getGroupScheduleByCode($thn_ajaran,$smt,$kode_kel)->row();
+		$jadwal = $this->M_jadwal->getJadwalByIdentitas($thn_ajaran,$smt,$jad_kelompok->ruang,$jad_kelompok->tgl,$jad_kelompok->mulai,$jad_kelompok->akhir)->result();
 
-		$this->load->view('template',$data);
-	}
 
-	public function hearingScheduleDetail($thn_ajaran = NULL, $smt = NULL, $ruang = NULL, $tgl = NULL, $mulai = NULL, $akhir = NULL) {
-		if ( ! $this->session->userdata('isParticipantTps') ) {
-			redirect('public/home','refresh');
+		$dataJadwal = [];
+		foreach ($jadwal as $value) {
+			$jadwal_n = [];
+			
+			// jadwal
+			$moderator = $this->M_dosen->getDosenBynpp($value->moderator)->row()->nama;
+			$penguji1 = $this->M_dosen->getDosenBynpp($value->penguji1)->row()->nama;
+			$penguji2 = $this->M_dosen->getDosenBynpp($value->penguji2)->row()->nama;
+			$tgl = new DateTime($value->tgl);
+			$mulai = $value->mulai <= 9 ? '0'. $value->mulai .':00' : $value->mulai .':00';
+			$akhir = $value->akhir <= 9 ? '0'. $value->akhir .':00' : $value->akhir .':00';
+			$waktu = $mulai.' - '.$akhir;
+
+			// anggota
+			$anggota = $this->M_anggota->getAnggotaByKodeKelJoinMhs($setting->thn_ajaran, $setting->smt, $value->kode_kel)->result();
+
+			$anggotanya = [];
+			foreach ($anggota as $dataAnggota) {
+				$anggota_n = [];
+
+				$anggota_n = [
+					'nbi'	=> $dataAnggota->nbi,
+					'nama'	=> $dataAnggota->nama
+				];
+
+				$anggotanya[] = $anggota_n;
+			}
+
+			$jadwal_n = [
+				'tanggal'		=> $tgl->format('d-m-Y'),
+				'waktu'			=> $waktu,
+				'ruang'			=> $value->ruang, 
+				'moderator'		=> $moderator,
+				'penguji1'		=> $penguji1,
+				'penguji2'		=> $penguji2,
+				'kode_kel'		=> $value->kode_kel,
+				'anggotanya'	=> $anggotanya
+			];
+
+			$dataJadwal[] = $jadwal_n;
 		}
-
-		if ( $thn_ajaran == NULL || $smt == NULL || $ruang == NULL || $tgl == NULL || $mulai == NULL || $akhir == NULL  ) {
-			redirect('participant/schedule/hearingSchedule','refresh');
-		}
-
-		$setting = $this->M_setting->getSetting()->row();
-
-		if ( $thn_ajaran != $setting->thn_ajaran && $thn_ajaran != $setting->smt ) {
-			redirect('participant/schedule/hearingSchedule','refresh');
-		}
-
-		$identitasJadwal = $this->M_jadwal->getJadwalByIdentitas($thn_ajaran,$smt,$ruang,$tgl,$mulai,$akhir)->row();
-		$detailJadwal = $this->M_jadwal->getJadwalByIdentitas($thn_ajaran,$smt,$ruang,$tgl,$mulai,$akhir)->result();
-		if ( !$identitasJadwal || !$detailJadwal ) {
-			redirect('participant/schedule/hearingSchedule','refresh');
-		}
-
-		$moderator = $this->M_dosen->getDosenByNpp($identitasJadwal->moderator)->row()->nama;
-		$penguji1 = $this->M_dosen->getDosenByNpp($identitasJadwal->penguji1)->row()->nama;
-		$penguji2 = $this->M_dosen->getDosenByNpp($identitasJadwal->penguji2)->row()->nama;
 
 		$data = [
 			'settingData'		=> $setting,
-			'moderator'			=> $moderator,
-			'penguji1'			=> $penguji1,
-			'penguji2'			=> $penguji2,
-			'identitasJadwal'	=> $identitasJadwal,
-			'detailJadwal'		=> $detailJadwal,
-			'content' 			=> 'participant/schedule/hearing_schedule_detail',
-			'pagetitle' 		=> 'Detail Jadwal Sidang',
-			'navbartitle' 		=> 'Detail Jadwal Sidang'
+			'dataJadwal'		=> $dataJadwal,
+			'content' 			=> 'participant/schedule/hearing_schedule_lists',
+			'pagetitle' 		=> 'Jadwal Sidang',
+			'navbartitle' 		=> 'Jadwal Sidang'
 		];
 
 		$this->load->view('template',$data);
 	}
-
 }
 
 /* End of file Schedule.php */
