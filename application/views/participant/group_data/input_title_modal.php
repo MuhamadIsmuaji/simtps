c3341bff264471454658ea1b336db4c79ce88681
MuhamadIsmuaji <muhamadismuaji@gmail.com>
2016-05-27 20:15:08 +0700

    perbaikan input judul dihalaman peserta

--- application/views/participant/group_data/input_title_modal.php
+++ application/views/participant/group_data/input_title_modal.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
-<div class="modal fade modal-primary" id="input_title_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static">
-    <div class="modal-dialog">
+<div class="modal fade modal-primary bs-example-modal-lg" id="input_title_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -10,7 +10,8 @@
             <div class="modal-body">
                 <div class="form-group">
                     <label for="exampleInputEmail1">Judul Tugas Perancangan Sistem</label>
-                    <input type="text" class="form-control" name="judul" placeholder="Judul Tugas Perancangan Sistem" required />
+                    <!-- <input type="text" class="form-control" name="judul" placeholder="Judul Tugas Perancangan Sistem" required /> -->
+                    <textarea class="form-control" name="judul" rows="4"></textarea>
                     <input type="hidden" id="kode_kel_modal_title" name="kode_kel_modal_title" />
                     <p class="help-block"><strong><i>*Judul yang sudah ada akan diganti</i></strong></p>
                 </div>
